d9c3eb4f54bdb383e7ba35b85f1ce3c1f32a2f59
Pavel Stoyanov <114023028+PavelStoyanov06@users.noreply.github.com>
2024-11-07 00:47:34 +0200

    Added ApplicationUser

---
+++ LitConnect/LitConnect.Common/ValidationConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace LitConnect.Common;
+
+public static class ValidationConstants
+{
+    public static class User
+    {
+        public const int FirstNameMinLength = 2;
+        public const int FirstNameMaxLength = 50;
+        public const int LastNameMinLength = 2;
+        public const int LastNameMaxLength = 50;
+    }
+}
---
+++ LitConnect/LitConnect.Data.Models/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿namespace LitConnect.Data.Models;
+
+using LitConnect.Common;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+public class ApplicationUser : IdentityUser
+{
+    [Required]
+    [MinLength(ValidationConstants.User.FirstNameMinLength)]
+    [MaxLength(ValidationConstants.User.FirstNameMaxLength)]
+    public string FirstName { get; set; } = null!;
+
+    [Required]
+    [MinLength(ValidationConstants.User.LastNameMinLength)]
+    [MaxLength(ValidationConstants.User.LastNameMaxLength)]
+    public string LastName { get; set; } = null!;
+
+    public bool IsDeleted { get; set; }
+}
