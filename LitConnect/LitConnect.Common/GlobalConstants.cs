0ca882644880b9fb2882175c3525d9abb666cc4f
Pavel Stoyanov <114023028+PavelStoyanov06@users.noreply.github.com>
2024-11-07 00:49:27 +0200

    added global constants

---
+++ LitConnect/LitConnect.Common/GlobalConstants.cs
@@ -0,0 +1,16 @@
+﻿namespace LitConnect.Common;
+
+public static class GlobalConstants
+{
+    public static class SystemConstants
+    {
+        public const string SystemName = "LitConnect";
+        public const string AdminAreaName = "Admin";
+    }
+
+    public static class RoleConstants
+    {
+        public const string AdministratorRoleName = "Administrator";
+        public const string UserRoleName = "User";
+    }
+}
