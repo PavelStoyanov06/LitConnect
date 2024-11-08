f58198bb98ac89ada9e8ad2113076af5b91a8651
Pavel Stoyanov <114023028+PavelStoyanov06@users.noreply.github.com>
2024-11-09 00:38:38 +0200

    added login view model

---
+++ LitConnect/LitConnect.Web.ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class LoginViewModel
+{
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Invalid email address")]
+    public required string Email { get; set; }
+
+    [Required(ErrorMessage = "Password is required")]
+    [DataType(DataType.Password)]
+    public required string Password { get; set; }
+
+    [Display(Name = "Remember me?")]
+    public bool RememberMe { get; set; }
+}
