0f8814d7e1a965ce24b892c30bbeaadae80feaf5
Pavel Stoyanov <114023028+PavelStoyanov06@users.noreply.github.com>
2024-11-23 01:48:32 +0200

    Added Book All View Model

---
+++ LitConnect/LitConnect.Web.ViewModels/Book/BookAllViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace LitConnect.Web.ViewModels.Book;
+
+public class BookAllViewModel
+{
+    public string Id { get; set; } = null!;
+
+    public string Title { get; set; } = null!;
+
+    public string Author { get; set; } = null!;
+
+    public ICollection<string> Genres { get; set; } = new HashSet<string>();
+}
