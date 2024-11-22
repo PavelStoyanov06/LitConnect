f529f3ba543f0b895f75edb500b226e6b8c6154b
Pavel Stoyanov <114023028+PavelStoyanov06@users.noreply.github.com>
2024-11-23 01:46:55 +0200

    Added Book Creation View Model

---
+++ LitConnect/LitConnect.Web.ViewModels/Book/BookCreateViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace LitConnect.Web.ViewModels.Book;
+
+using System.ComponentModel.DataAnnotations;
+using static LitConnect.Common.ValidationConstants.Book;
+
+public class BookCreateViewModel
+{
+    [Required]
+    [StringLength(TitleMaxLength, MinimumLength = TitleMinLength,
+        ErrorMessage = "Title must be between {2} and {1} characters")]
+    public string Title { get; set; } = null!;
+
+    [Required]
+    [StringLength(AuthorMaxLength, MinimumLength = AuthorMinLength,
+        ErrorMessage = "Author name must be between {2} and {1} characters")]
+    public string Author { get; set; } = null!;
+
+    [StringLength(DescriptionMaxLength)]
+    public string? Description { get; set; }
+
+    [Display(Name = "Genres")]
+    public required IEnumerable<string> GenreIds { get; set; }
+}
