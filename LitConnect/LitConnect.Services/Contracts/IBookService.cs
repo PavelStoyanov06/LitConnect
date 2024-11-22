bf1a4940c97580f1a2564560222d188e206c3761
Pavel Stoyanov <114023028+PavelStoyanov06@users.noreply.github.com>
2024-11-23 01:52:27 +0200

    added book service contract

---
+++ LitConnect/LitConnect.Services/Contracts/IBookService.cs
@@ -0,0 +1,21 @@
+﻿namespace LitConnect.Services.Contracts;
+
+using LitConnect.Web.ViewModels.Book;
+using LitConnect.Web.ViewModels.Genre;
+
+public interface IBookService
+{
+    Task<IEnumerable<BookAllViewModel>> GetAllAsync();
+
+    Task<BookDetailsViewModel?> GetDetailsAsync(string id);
+
+    Task<string> CreateAsync(BookCreateViewModel model);
+
+    Task<IEnumerable<GenreViewModel>> GetAllGenresAsync();
+
+    Task AddToBookClubAsync(string bookId, string bookClubId);
+
+    Task RemoveFromBookClubAsync(string bookId, string bookClubId);
+
+    Task<bool> ExistsAsync(string id);
+}
