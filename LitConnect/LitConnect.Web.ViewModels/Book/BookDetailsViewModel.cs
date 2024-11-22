9b1ffd0c4337d1737d682ce4a5b49eb65465d437
Pavel Stoyanov <114023028+PavelStoyanov06@users.noreply.github.com>
2024-11-23 01:47:43 +0200

    Added Book Details View Model

---
+++ LitConnect/LitConnect.Web.ViewModels/Book/BookDetailsViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace LitConnect.Web.ViewModels.Book;
+
+public class BookDetailsViewModel
+{
+    public string Id { get; set; } = null!;
+
+    public string Title { get; set; } = null!;
+
+    public string Author { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public ICollection<string> Genres { get; set; } = new HashSet<string>();
+
+    public int BookClubsCount { get; set; }
+}
