ac1202010c97eb5b923bb6ef77178dd75c862af3
Pavel Stoyanov <114023028+PavelStoyanov06@users.noreply.github.com>
2024-11-23 18:46:38 +0200

    Added genre seeding.

---
+++ LitConnect/LitConnect.Data/Seeding/GenreSeeder.cs
@@ -0,0 +1,35 @@
+﻿namespace LitConnect.Data.Seeding;
+
+using LitConnect.Data.Models;
+
+public class GenreSeeder
+{
+    public static async Task SeedAsync(LitConnectDbContext dbContext)
+    {
+        if (dbContext.Genres.Any())
+        {
+            return;
+        }
+
+        var genres = new List<Genre>()
+        {
+            new Genre { Name = "Fiction" },
+            new Genre { Name = "Non-Fiction" },
+            new Genre { Name = "Mystery" },
+            new Genre { Name = "Science Fiction" },
+            new Genre { Name = "Fantasy" },
+            new Genre { Name = "Romance" },
+            new Genre { Name = "Thriller" },
+            new Genre { Name = "Horror" },
+            new Genre { Name = "Historical Fiction" },
+            new Genre { Name = "Biography" },
+            new Genre { Name = "Self-Help" },
+            new Genre { Name = "Business" },
+            new Genre { Name = "Science" },
+            new Genre { Name = "Poetry" }
+        };
+
+        await dbContext.Genres.AddRangeAsync(genres);
+        await dbContext.SaveChangesAsync();
+    }
+}
