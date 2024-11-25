a2c587d20c176e71a6262fa3fae93ac59afd6cce
Pavel Stoyanov <114023028+PavelStoyanov06@users.noreply.github.com>
2024-11-26 00:38:57 +0200

    added extra book models to book club

---
+++ LitConnect/LitConnect.Web.ViewModels/BookClub/AddBookViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace LitConnect.Web.ViewModels.BookClub;
+
+public class AddBookViewModel
+{
+    public string BookClubId { get; set; } = null!;
+
+    public required string BookId { get; set; }
+
+    public bool IsCurrentlyReading { get; set; }
+}
---
+++ LitConnect/LitConnect.Web.ViewModels/BookClub/BookClubBookViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace LitConnect.Web.ViewModels.BookClub;
+
+public class BookClubBookViewModel
+{
+    public string Id { get; set; } = null!;
+
+    public string Title { get; set; } = null!;
+
+    public string Author { get; set; } = null!;
+
+    public bool IsCurrentlyReading { get; set; }
+
+    public IEnumerable<string> Genres { get; set; } = new List<string>();
+}
