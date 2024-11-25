73fe19ba00f259ba62eed9fdf7d717215cc77350
Pavel Stoyanov <114023028+PavelStoyanov06@users.noreply.github.com>
2024-11-25 23:13:10 +0200

    added meeting service contract

---
+++ LitConnect/LitConnect.Services/Contracts/IMeetingService.cs
@@ -0,0 +1,16 @@
+﻿namespace LitConnect.Services.Contracts;
+
+using LitConnect.Web.ViewModels.Meeting;
+
+public interface IMeetingService
+{
+    Task<IEnumerable<MeetingInListViewModel>> GetBookClubMeetingsAsync(string bookClubId);
+
+    Task<MeetingDetailsViewModel?> GetDetailsAsync(string id);
+
+    Task<string> CreateAsync(MeetingCreateViewModel model);
+
+    Task DeleteAsync(string id);
+
+    Task<bool> ExistsAsync(string id);
+}
