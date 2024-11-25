10751155c08f73eac714ce5da6179ffcb88769aa
Pavel Stoyanov <114023028+PavelStoyanov06@users.noreply.github.com>
2024-11-25 23:11:03 +0200

    Created Meeting View Model

---
+++ LitConnect/LitConnect.Web.ViewModels/Meeting/MeetingCreateViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace LitConnect.Web.ViewModels.Meeting;
+
+using System.ComponentModel.DataAnnotations;
+using static LitConnect.Common.ValidationConstants.Meeting;
+
+public class MeetingCreateViewModel
+{
+    [Required]
+    [StringLength(TitleMaxLength, MinimumLength = TitleMinLength,
+        ErrorMessage = "Title must be between {2} and {1} characters")]
+    public string Title { get; set; } = null!;
+
+    [StringLength(DescriptionMaxLength)]
+    public string? Description { get; set; }
+
+    [Required]
+    [Display(Name = "Meeting Date and Time")]
+    public DateTime ScheduledDate { get; set; }
+
+    [Required]
+    public string BookClubId { get; set; } = null!;
+
+    public string? BookId { get; set; }
+}
