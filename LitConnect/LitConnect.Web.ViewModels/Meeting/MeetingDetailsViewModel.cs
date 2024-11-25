a061af6c3fe1733acd50789de3a3c31e81619fac
Pavel Stoyanov <114023028+PavelStoyanov06@users.noreply.github.com>
2024-11-25 23:12:22 +0200

    Added two more view models

---
+++ LitConnect/LitConnect.Web.ViewModels/Meeting/MeetingDetailsViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace LitConnect.Web.ViewModels.Meeting;
+
+public class MeetingDetailsViewModel
+{
+    public string Id { get; set; } = null!;
+
+    public string Title { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public DateTime ScheduledDate { get; set; }
+
+    public string BookClubId { get; set; } = null!;
+
+    public string BookClubName { get; set; } = null!;
+
+    public string? BookTitle { get; set; }
+
+    public bool IsUpcoming => ScheduledDate > DateTime.UtcNow;
+}
---
+++ LitConnect/LitConnect.Web.ViewModels/Meeting/MeetingInListViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace LitConnect.Web.ViewModels.Meeting;
+
+public class MeetingInListViewModel
+{
+    public string Id { get; set; } = null!;
+
+    public string Title { get; set; } = null!;
+
+    public DateTime ScheduledDate { get; set; }
+
+    public string? BookTitle { get; set; }
+
+    public bool IsUpcoming => ScheduledDate > DateTime.UtcNow;
+}
